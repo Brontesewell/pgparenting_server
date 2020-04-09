664c48aad29a9fcf62ebc4cf78778843bfb72354
Brontesewell <brontsewell7@gmail.com>
2020-04-08 20:59:25 -0700

    add subscirbe to users migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_12_220927) do
+ActiveRecord::Schema.define(version: 2020_03_12_220999) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -156,6 +156,7 @@ ActiveRecord::Schema.define(version: 2020_03_12_220927) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "subscribe"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
