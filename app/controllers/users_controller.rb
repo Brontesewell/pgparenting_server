f6da73805cd9d09c2048c848a93d6f1836fe2968
Brontesewell <brontsewell7@gmail.com>
2020-04-09 17:08:40 -0700

    subscribe controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,7 +51,7 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:id, :first_name, :last_name, :email, :password)
+        params.require(:user).permit(:id, :first_name, :last_name, :email, :password, :subscribe)
     end
 
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-    attributes :id, :first_name, :last_name, :courses, :email
+    attributes :id, :first_name, :last_name, :courses, :email, :subscribe
     # attribute :room do |user|
     #     user.room.uniq
     #   end
